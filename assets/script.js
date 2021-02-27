322a05349fa9a0c59f072babc40a16d052cb3682
Nate Kent <ngkent75@gmail.com>
2021-02-27 12:31:11 -0500

    Wiki media populates in body

--- assets/script.js
+++ assets/script.js
@@ -2,16 +2,17 @@
 // Declared variables
 var keyA = '599c1b07';
 var keyB = '15010a1a6526890db1028fed6712f26f';
-var userInput = 'Jack Reacher';
+var userInput = 'John Wick';
 var movieURL = 'https://www.omdbapi.com/?apikey=' + keyA + '&t=' + userInput;
 var wikiPath = userInput.split(' ').join('%');
 var encodedName = encodeURIComponent(userInput + ' (film)')
-console.log(encodedName);
 
-var testWiki = 'https://en.wikipedia.org/w/api.php?action=query&titles=' + encodedName + '&format=json&origin=*&prop=links'
+var wikiURL = 'https://en.wikipedia.org/w/api.php?action=query&titles=' + encodedName + '&format=json&origin=*&prop=links'
 
 // Fetch request for OMDB. Creates element for title for now.
-fetch (movieURL)
+
+function movieFetch() {
+    fetch (movieURL)
     .then (function (response) {
         return response.json();
     })
@@ -22,26 +23,36 @@ fetch (movieURL)
         document.body.append(newTitle)
         // Starts fetches for poster after so the poster generates underneath.
         wikiFetch();
-});
+    });
+}
 
+movieFetch();
 
+// Fetches wikipedia related media
 function wikiFetch() {
 
-    fetch (testWiki)
+    fetch (wikiURL)
     .then (function (response) {
         return response.json();
     })
     .then (function (data) {
         console.log(data);
         var wikiID = data.continue.plcontinue.split('|')[0]
-        console.log(wikiID);
-        var relatedMedia = data.query.pages.wikiID.links
-        console.log(relatedMedia);
+
+        var uList = document.createElement('ul')
+        uList.id = 'mediaList'
+        document.body.append(uList)
+        var relatedMedia = data.query.pages[wikiID]['links']
+        for (i = 0; i < relatedMedia.length; i++) {
+          var lItem = document.createElement('li')
+          lItem.textContent = relatedMedia[i]['title']
+          uList.append(lItem)
+        }
+
         
     });
 };
 
-// script goes here
 $('[data-app-dashboard-toggle-shrink]').on('click', function(e) {
     e.preventDefault();
     $(this).parents('.app-dashboard').toggleClass('shrink-medium').toggleClass('shrink-large');
